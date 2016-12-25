8c3d79587ba3864b230e3f6ee1a2e6026cedaf42
Akash Sethi <akashs@USCorp.AudSci.com>
2016-12-25 16:20:33 +0530

    resolved java file

--- app/src/main/java/com/shopkeeperhelper/MainActivity.java
+++ app/src/main/java/com/shopkeeperhelper/MainActivity.java
@@ -9,11 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-=======
 
         //First get data from database, create bean and render data in listview
         //If no data then show message
->>>>>>> 49544d5142545b8e8c405b362a186d87579264d4
+
     }
 }
